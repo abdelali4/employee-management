6ad7b87df893bf92d81b9f50b6f345a0532cda6e
Abdelali EL hammadi <79413375+abdelali4@users.noreply.github.com>
2022-05-14 15:22:37 +0100

    Update App.js

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="container">
             <div className="row justify-content-center">
-                {/* components goes here */}
+                
                 <Table />
             </div>
         </div>
